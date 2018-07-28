1cbd01c1841e8371b2e66aef9fdc975c4009271a
aqweteddy <keddy940199@gmail.com>
2018-07-28 16:27:49 +0800

    add crawler

---
+++ crawler/crawler.py
@@ -0,0 +1,157 @@
+import os
+import json
+import urllib
+import jieba as jb
+
+
+class ThreadList(list):
+    def find_comment_by_user(self, pat):
+        return [com for th in self for com in th['comment'] if pat in com['user']]
+
+    def get_img_link(self):
+        """
+        return a list (title, img_link)
+        -> [0] : title
+        -> [1] : list of img_link
+        -> [2] : URL
+        """
+        return [{'title': th['title'], 'img_link': th['img_link'], 'id': j, 'url': th['url']}
+                for j, th in enumerate(self)]
+        # return [(th['title'], th['img_link'], th['url'] for th in self]
+
+
+# scrapy crawl ptt -o beauty.json -a board=Beauty -a  pages=3
+
+
+class ToolBox:
+    def __init__(self, board='Gossiping', pages=1, file='tmp.json', title_lim=[], jsonf=None):
+        if not jsonf:
+            os.chdir(os.path.split(os.path.realpath(__file__))[0])
+            com = 'scrapy crawl ptt '
+            com += '-o %s ' % (file)
+            com += '-a pages=%d ' % (pages)
+            com += '-a board=%s ' % (board)
+
+            if title_lim:
+                com += '-a title_lim="'
+                for lim in title_lim:
+                    com += "%s," % (str(lim))
+                com += '" '
+            print('Command: ' + com)
+            os.system(f'''rm -f {file}''')
+            os.system(f'''{com}''')
+            os.chdir('..')
+        else:
+            file = jsonf
+
+        self.data = ThreadList()
+        self.load_json(file)
+
+    def get_title(self):
+        return [i['title'] for i in self.data]
+
+    def load_json(self, file):
+        """
+        load_json(NAME)
+        load json as name
+        """
+        with open(file, 'r') as f:
+            self.data.extend(json.load(f))
+
+    def save_json(self, file):
+        """
+        save_json(NAME)
+        save json as NAME
+        """
+        with open(file, 'w', encoding='utf8') as f:
+            json.dump(self.data, f)
+
+    def download_image(self, name):
+        """
+        Download imgur pictures in ./pictures<pid>
+        classified by folder
+        """
+        if not os.path.exists(name):
+            os.mkdir(name)
+        for num, th in enumerate(self.data.get_img_link()):
+            dirr = name + '/' + th['title'].replace('/', '.')
+
+            if not os.path.exists(dirr):
+                os.mkdir(dirr)
+            yield num+1, th['title']
+
+            for i, link in enumerate(th['img_link']):
+                link = link.replace('https', 'http')
+
+                try:
+                    urllib.request.urlretrieve(link, dirr + '/' + str(i + 1) + '.jpg')
+                except:
+                    print('Get image error: %s in %s' % (link, th['title']))
+
+
+class ToolBoxTextAnalysis(ToolBox):
+    def cut_text(self):
+        '''
+        Use Jieba to cut text
+        '''
+        jb.set_dictionary('./dict.txt.big')
+        jb.load_userdict('./ptt_custom.dict')
+        res = []
+        for ar in self.data:
+            res.append([i for i in jb.cut(ar['text'].strip('\n'), cut_all=True)
+                        if not i.isspace()])
+
+        self.cut_res_text = res
+        return res
+
+    def cut_comment_text(self):
+        """
+        Use Jieba to cut comment
+        """
+        res = []
+        for ar in self.data:
+            for com in ar['comment']:
+                res.append([i for i in jb.cut(com['text'].strip(''))
+                            if len(i) > 1])
+        self.cut_res_com = res
+        return res
+
+    def get_text_freq_sorted(self):
+        """
+        return dict to distribute cut result.
+        if you don't call cut_text() before, get_freq_sorted() will be auto call cut_text() first.
+        """
+        try:
+            self.cut_res_text
+        except AttributeError:
+            self.cut_text()
+
+        dic = dict()
+        for i in self.cut_res_text:
+            for j in i:
+                dic[j] = 1 if j not in dic else dic[j] + 1
+        return sorted(dic.items(), key=lambda kv: kv[1], reverse=True)
+
+    def get_comment_freq_sorted(self):
+        try:
+            self.cut_res_com
+        except AttributeError:
+            self.cut_comment_text()
+
+        dic = dict()
+        for i in self.cut_res_com:
+            for j in i:
+                dic[j] = 1 if j not in dic else dic[j] + 1
+        return sorted(dic.items(), key=lambda kv: kv[1], reverse=True)
+
+
+if __name__ == '__main__':
+    a = ToolBoxTextAnalysis(json='beauty.json')
+    # a = ToolBoxTextAnalysis(board='Gossiping', pages=2, file='gossip.json', title_lim=['-', '公告'])
+    # a = ToolBoxTextAnalysis(board='Baseball', pages=2, file='baseball.json', title_lim=['-', '公告'])
+    # a = ToolBoxTextAnalysis(board='Beauty', pages=2, file='beauty.json', title_lim=['-', '公告', '帥哥'])
+    for i in a.download_image():
+        print(i)
+    # for i in a.get_freq_sorted():
+    #    print(i)
+    # a = ToolBox(json='tmp.json')
---
+++ crawler/ptt_crawl/spiders/ptt.py
@@ -0,0 +1,121 @@
+import scrapy
+from scrapy.selector import Selector
+from ptt_crawl.items import PttCrawlItem
+from datetime import datetime
+
+
+def filter(limits, text):
+    if limits == []:
+        return True
+    if limits[0] == '+':
+        for lim in limits[1:]:
+            if lim in text:
+                return True
+        return False
+    else:  # -
+        for lim in limits[1:]:
+            if lim in text:
+                return False
+        return True
+
+
+class PttSpiderByPage(scrapy.Spider):
+    name = 'ptt'
+    allowed_domains = ['ptt.cc']
+    now_pages = 0
+
+    def __init__(self, *args, **kwargs):
+        super(PttSpiderByPage, self).__init__(*args, **kwargs)
+        self.item = PttCrawlItem()
+        self.title_lim = []
+
+        self.start_urls = ['https://www.ptt.cc/bbs/%s/index.html' %
+                           (kwargs['board'])] if 'board' in kwargs.keys() else []
+        self.title_lim = kwargs['title_lim'].split(
+            ',')[0:-1] if 'title_lim' in kwargs.keys() else []
+        self.MAX_PAGES = int(
+            kwargs['pages']) if 'pages' in kwargs.keys() else 1
+
+    def start_requests(self):
+        yield scrapy.Request(self.start_urls[0], cookies={'over18': '1'})
+
+    def parse(self, resp):
+        self.now_pages += 1
+        print('Now at %s, page #%d' %
+              (str(resp).split(' ')[1][:-2], self.now_pages))
+
+        for arti in resp.xpath('//div[@class="r-ent"]/div[@class="title"]/a'):
+            title = arti.xpath('text()').extract()[0]
+            url = resp.urljoin(arti.xpath('@href').extract()[0])
+
+            if filter(self.title_lim, title):
+                print('Parsing: %s\t%s' % (title, url))
+                yield scrapy.Request(url=url, cookies={'over18': '1'}, callback=self.parse_post)
+            else:
+                print('Exclude: %s\t%s' % (title, url))
+
+        if self.now_pages < self.MAX_PAGES:
+            next_page = resp.xpath(
+                '//div[@id="action-bar-container"]//a[contains(text(), "上頁")]/@href')
+            if next_page:
+                url = resp.urljoin(next_page[0].extract())
+                yield scrapy.Request(url=url, cookies={'over18': '1'}, callback=self.parse)
+
+    def parse_post(self, resp):
+        tmp = resp.xpath(
+            '//meta[@property="og:title"]/@content')[0].extract()
+        try:
+            self.item['title'] = tmp
+        except IndexError:
+            self.item['title'] = ""
+        except:
+            self.item['title'] = 'err'
+        try:
+
+            self.item['category'] = 'Re' if 'Re:' in tmp else tmp.split(']')[0][1:].strip()
+        except IndexError:
+            self.item['category'] = 'not found'
+        except:
+            self.item['category'] = 'err'
+
+        self.item['text'] = resp.xpath(
+            '//div[@id="main-content"]/text()')[0].extract()
+        self.item['author'] = resp.xpath(
+            '//div[@class="article-metaline"]/span[text()="作者"]/following-sibling::span[1]/text()')[0].extract()
+        self.item['img_link'] = []
+        for link in resp.xpath('//div[@id="main-content"]/a/@href'):
+            tmp = link.extract()
+            if '.jpg' in tmp.split('/')[-1] or '.png' in tmp.split('/')[-1] \
+                    or '.gif' in tmp.split('/')[-1]:
+                self.item['img_link'].append(tmp)
+            elif 'imgur' in tmp:
+                tmp = 'http://i.imgur.com/' + link.extract().split('/')[-1]
+                if '.jpg' not in tmp:
+                    tmp += '.jpg'
+                self.item['img_link'].append(tmp)
+
+
+
+        date_str = resp.xpath(
+            '//div[@class="article-metaline"]/span[text()="時間"]/following-sibling::span[1]/text()')[0].extract()
+        self.item['date'] = datetime.strptime(date_str, '%a %b %d %H:%M:%S %Y')
+
+        for f2 in resp.xpath('//div[@id="main-content"]/span[@class="f2"]/text()'):
+            if '※ 發信站: 批踢踢實業坊(ptt.cc), 來自: ' in f2.extract():
+                self.item['ip_author'] = f2.extract().strip().split(': ')[-1]
+                break
+
+        comment = []
+
+        for com in resp.xpath('//div[@class="push"]'):
+            tag = com.css('span.push-tag::text')[0].extract().strip()
+            user = com.css('span.push-userid::text')[0].extract().strip()
+            text = com.css('span.push-content::text')[0].extract().strip()[2:]
+            ip = com.css('span.push-ipdatetime::text')[0].extract().strip().lstrip(' ')
+            ip = ip.split(' ')[0] if '.' in ip else ''
+
+            comment.append({'tag': tag, 'user': user, 'text': text, 'ip': ip})
+
+        self.item['url'] = resp.url
+        self.item['comment'] = comment
+        yield self.item
